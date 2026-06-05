264becf1759f5a8a892daf9a4cb52a97bbb72552
Logan Smith <iamlogansmith@gmail.com>
2026-06-05 15:03:20 -0400

    fix: explicitly use venv python path to prevent miniconda fallback

--- start.js
+++ start.js
@@ -7,7 +7,7 @@ module.exports = {
         venv: "../env",
         path: "app",
         message: [
-          "python -u -c \"exec('''import subprocess, os\\nRF = 'RESTART_REQUIRED'\\nCMD = ['python', '-m', 'uvicorn', 'app.main:app', '--host', '127.0.0.1', '--port', '7070']\\nsubprocess.run(CMD)\\nwhile os.path.exists(RF):\\n    os.remove(RF)\\n    subprocess.run(CMD)''')\""
+          "python -u -c \"exec('''import subprocess, os, sys\\nRF = 'RESTART_REQUIRED'\\nvenv_py = os.path.abspath(os.path.join('..', 'env', 'Scripts', 'python.exe')) if os.name == 'nt' else os.path.abspath(os.path.join('..', 'env', 'bin', 'python'))\\nCMD = [venv_py, '-m', 'uvicorn', 'app.main:app', '--host', '127.0.0.1', '--port', '7070']\\nsubprocess.run(CMD)\\nwhile os.path.exists(RF):\\n    os.remove(RF)\\n    subprocess.run(CMD)''')\""
         ],
         on: [{
           event: "/(http:\\/\\/[^\\s]+)/",
