06ef8888b9c0821608a0950eddf430ae98081454
saintbrodie <iamlogansmith@gmail.com>
2026-09-25 15:22:19 -0400

    Use app-root Orange venv in managed install

--- install-managed.js
+++ install-managed.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       method: "shell.run",
       params: {
-        venv: "../env",
+        venv: "env",
         venv_python: "3.12",
         path: "app",
         message: [
